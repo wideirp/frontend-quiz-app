5cb32e1ea53325e969ed46b42b8bdd6ba4d1471e
wideirp <51214984+wideirp@users.noreply.github.com>
2024-09-02 16:51:53 -0500

    init commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import "normalize.css";
+import "@/assets/sass/global.scss";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");

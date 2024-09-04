74972e0068507a0c9f6155e66ae82df48f780515
wideirp <51214984+wideirp@users.noreply.github.com>
2024-09-03 22:02:22 -0500

    add: quiz header component

---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,32 @@
+<script setup>
+import CheckboxSlider from "./widgets/CheckboxSlider.vue";
+</script>
+
+<template>
+  <header>
+    <div class="theme">
+      <picture>
+        <img src="/icon-sun-dark.svg?image" alt="sun" />
+      </picture>
+      <CheckboxSlider bgUnchecked="#a729f5" bgChecked="#a729f5" />
+      <picture>
+        <img src="/icon-moon-dark.svg" alt="moon" />
+      </picture>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@use "@/assets/sass/variables" as *;
+
+header {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.theme {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+</style>

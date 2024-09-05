7d5cc28cb08880f623c157c225e1df85dc9d2f76
wideirp <51214984+wideirp@users.noreply.github.com>
2024-09-05 15:35:24 -0500

    add: images and button hover effect

--- src/components/QuizSelector.vue
+++ src/components/QuizSelector.vue
@@ -24,15 +24,19 @@ button {
   gap: 1rem;
   border-radius: 8px;
   background-color: $clr-btn-primary-light;
-  border: none;
+  border: 2px solid $clr-btn-primary-light;
   outline: none;
   padding: 0.5rem;
   font-size: 1.1rem;
   font-weight: 500;
-  // box-shadow: 0px 1px 5px #8fa0c144;
   &.dark-theme {
     background-color: $clr-btn-primary-dark;
+    border: 2px solid $clr-btn-primary-dark;
     color: $fc-primary-dark;
+  }
+
+  &:hover {
+    border-color: $clr-purple;
   }
 }
 </style>

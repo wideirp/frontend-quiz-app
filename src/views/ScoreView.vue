c6db7826f2241e8a84fcb158a75325e11dc5229c
wideirp <51214984+wideirp@users.noreply.github.com>
2024-09-08 13:29:28 -0500

    add: score view html and scss

--- src/views/ScoreView.vue
+++ src/views/ScoreView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useRouter } from "vue-router";
-import { homeStore, QuizStore, themeStore } from "@/assets/js/store";
+import { QuizStore, themeStore } from "@/assets/js/store";
+import SvgIcon from "@/components/widgets/SvgIcon.vue";
 
 const router = useRouter();
 
@@ -8,27 +9,31 @@ if (QuizStore.questionIndex < 10) {
   QuizStore.reset();
   router.push("/");
 }
-
-// function onAnswer(e) {
-//   if (QuizStore.selectedOption) {
-//     QuizStore.questionAnswered = true;
-//   } else {
-//     console.log("no option selected");
-//     QuizStore.emptyError = true;
-//   }
-// }
-
-// function onNext(e) {
-//   if (QuizStore.questionIndex >= 10) {
-//     router.push("/score");
-//   } else {
-//     QuizStore.nextQuestion();
-//   }
-// }
 </script>
 
 <template>
-  <h1>Score View</h1>
+  <section class="hero" :class="{ 'dark-theme': themeStore.darkTheme }">
+    <h1>Quiz completed <br /><span>You scored...</span></h1>
+  </section>
+
+  <section class="score">
+    <div class="scoreboard">
+      <div class="current-quiz">
+        <SvgIcon
+          :src="QuizStore.icon + '?image'"
+          :alt="QuizStore.title"
+          :bgColor="QuizStore.bgColor"
+          v-if="QuizStore.title"
+        />
+        <p v-if="QuizStore.title">{{ QuizStore.title }}</p>
+      </div>
+      <p class="score__result">{{ QuizStore.correct }}</p>
+      <p class="score__num-questions">
+        out of {{ QuizStore.questions.length }}
+      </p>
+    </div>
+    <button class="reset-btn">Play Again</button>
+  </section>
 </template>
 
 <style scoped lang="scss">
@@ -36,62 +41,72 @@ if (QuizStore.questionIndex < 10) {
 
 .hero {
   margin: 2rem 0;
-  .question-counter {
-    color: $fc-secondary-light;
-    font-size: $fs-200;
-    font-style: italic;
-    margin: 0;
-  }
-  .question {
+  h1 {
     color: $fc-primary-light;
-    font-size: $fs-400;
-    font-weight: $fw-medium;
-    margin: 1rem 0;
+    font-size: $fs-800;
+    font-weight: $fw-light;
+    margin: 0;
     margin-bottom: 1rem;
+    span {
+      font-weight: $fw-medium;
+      text-align: justify;
+    }
   }
   &.dark-theme {
-    .question-counter {
-      color: $fc-secondary-dark;
-    }
-    .question {
+    h1 {
       color: $fc-primary-dark;
     }
   }
 }
 
-.select {
-  .options {
+.score {
+  .scoreboard {
     display: flex;
     flex-direction: column;
-    gap: 0.75rem;
-  }
-  .select-btn {
-    width: 100%;
-    margin-top: 1rem;
-    padding: 1rem;
-    border-radius: 8px;
-    border: none;
-    outline: none;
-    background-color: $clr-purple;
-    color: $clr-pure-white;
-    font-weight: $fw-medium;
-    &:active {
-      transform: scale(0.98);
-      background-color: adjust-color($clr-purple, $lightness: -10%);
-    }
-  }
-
-  .no-answer {
-    display: flex;
-    align-items: center;
     justify-content: center;
-    gap: 0.5rem;
-    img {
-      width: 2rem;
+    align-items: center;
+    background-color: $clr-btn-primary-light;
+    padding: 2rem;
+    color: $fc-primary-light;
+    border-radius: 8px;
+    .current-quiz {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 1rem;
+      font-size: 1.1rem;
+      font-weight: 500;
+      p {
+        margin: 0;
+      }
     }
-    p {
-      color: $clr-red;
+
+    .score__result {
+      font-size: 5.5rem;
+      margin: 1rem 0;
+      font-weight: 500;
     }
+    .score__num-questions {
+      margin: 0;
+      color: $fc-secondary-light;
+      font-size: 1.1rem;
+    }
+  }
+}
+
+.reset-btn {
+  width: 100%;
+  margin-top: 1rem;
+  padding: 1rem;
+  border-radius: 8px;
+  border: none;
+  outline: none;
+  background-color: $clr-purple;
+  color: $clr-pure-white;
+  font-weight: $fw-medium;
+  &:active {
+    transform: scale(0.98);
+    background-color: adjust-color($clr-purple, $lightness: -10%);
   }
 }
 </style>
